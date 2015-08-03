d156d7b4dbc237e4405dbde1500432dea482db0c
Noly Khemin <nolykk@gmail.com>
2015-08-03 17:33:51 +0700

    fix: up button to act as back button

--- app/src/main/java/com/khemin/and/popularmovies/DetailActivity.java
+++ app/src/main/java/com/khemin/and/popularmovies/DetailActivity.java
@@ -17,7 +17,7 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -29,7 +29,11 @@ public class DetailActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+        if (id == android.R.id.home) {
+            onBackPressed();
             return true;
         }
 
